a51d414032e85749ded453ecb842cb871b71e05b
Fabian Kubon <fabian.kubon@hotmail.com>
2020-11-14 10:56:55 +0100

    route has uuid now

--- src/core/routes/api/IRoute.java
+++ src/core/routes/api/IRoute.java
@@ -1,8 +1,12 @@
 package core.routes.api;
 
+import java.util.UUID;
+
 import core.potholes.api.IPothole;
 
 public interface IRoute
 {
+	public UUID getUUID();
+	
 	public IPothole[] getPotholes();
 }
